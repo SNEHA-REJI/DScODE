da87a276b9bbeee5a4c660927e9943f4a560400b
BlueJ-14 <99087660+SNEHA-REJI@users.noreply.github.com>
2023-10-26 19:39:40 +0530

    Update PolyMultiply.c

--- PolyMultiply.c
+++ PolyMultiply.c
@@ -1,54 +1,24 @@
 
 #include<stdio.h>
 void InputPoly(int A[100],int t);
-void Polyadd(int A[100],int B[100],int C[100],int D[100]);
+void PolyMultiply(int A[100],int B[100],int C[100],int D[100]);
 void DisplayPoly(int A[100],int t);
-void Polyadd(int A[100],int B[100],int C[100],int D[100])
+void PolyMultiply(int A[100],int B[100],int C[100],int D[100]) 
 {
-int m,n,p=1,q=1,r=1,t,s,i;
+int m,n,q=1,p=1,r=1,i,s,t;
 m=A[0];
 n=B[0];
-while(p<=(2*m) && q<=(2*n))
-{
-if(A[p]==B[q])
-{
-C[r+1]=A[p+1]+B[q+1];
-if(C[r+1]!=0)
-{
-C[r]=A[p];
-r=r+2;
-}
-p=p+2;
-q=q+2;
-}
-else if(A[p]<B[q])
-{
-C[r+1]=B[q+1];
-C[r]=B[q];
-q=q+2;
-r=r+2;
-}
-else if(A[p]>B[q])
-{
-C[r+1]=A[p+1];
-C[r]=A[p];
-p=p+2;
-r=r+2;
-}
-}
 while(p<=(2*m))
 {
-C[r]=A[p];
-C[r+1]=A[p+1];
-p=p+2;
-r=r+2;
-}
-while(q<=(2*n))
-{
-C[r]=B[q];
-C[r+1]=B[q+1];
-q=q+2;
-r=r+2;
+    q=1;
+    while(q<=(2*n))
+    {
+      C[r]=A[p]+B[q];
+      C[r+1]=A[p+1]*B[q+1];
+      q=q+2;
+      r=r+2;
+    }
+    p=p+2;
 }
 t=r/2;
 r=1;
@@ -77,35 +47,17 @@ while(r<=(2*t))
    }
    r=r+2;
 }
-D[0]=s/2;
-printf("\nPolynomial Addition Result using arrray\n");
+printf("\nPolynomial Multiplication Result using arrray\n");
 DisplayPoly(D,(s-1));
 }
 void main()
 {
-int MA[100],MB[100],MC[100],MD[100],term,term1,term2,ch;
-printf("MENU\n");
-printf("1.Input and Display Polynomial using array\n");
-printf("2.Perform Polynomial addition of two polynomials and display\n");
-printf("Enter your choice\n");
-scanf("%d",&ch);
-switch(ch)
-{
-case 1:
-{
-InputPoly(MA,term);
-break;
-}
-case 2:
-{
+int MA[100],MB[100],MC[100],MD[100],term1,term2;
 printf("\nPolynomial 1\n");
 InputPoly(MA,term1);
 printf("\nPolynomial 2\n");
 InputPoly(MB,term2);
-Polyadd(MA,MB,MC,MD);
-break;
-}
-}
+PolyMultiply(MA,MB,MC,MD);
 }
 void InputPoly(int A[100],int t)
 {
@@ -126,7 +78,7 @@ void DisplayPoly(int A[100],int t)
 int i;
 for(i=1;i<=t;i=i+2)
 {
-printf("%dx^%d",A[i+1],A[i]);
+printf("(%dx^%d)",A[i+1],A[i]);
 if(i<(t-2))
 printf("+");
 }
