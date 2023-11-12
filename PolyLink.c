dec0b01290f7771d270df0311d6aec61434479ce
BlueJ-14 <99087660+SNEHA-REJI@users.noreply.github.com>
2023-11-12 20:16:50 +0530

    Update PolyLink.c

--- PolyLink.c
+++ PolyLink.c
@@ -6,19 +6,20 @@ struct node
  int expo;
  struct node *next;
 };
-struct node *p,*q,*r,*head1,*head2,*head3,*tail1,*tail2,*tail3,*ptr,*prev;
+struct node *p,*q,*r,*m,*head1,*head2,*head3,*tail1,*tail2,*tail3,*head4,*tail4,*ptr,*pre;
 void main()
 {
 head1=NULL;
 head2=NULL;
 head3=NULL;
+head4=NULL;
 int i=0,j=0,t1,t2,ch;
 do{
 printf("\nMENU\n");
 printf("1.Enter Polynomial 1 & Display\n");
 printf("2.Enter Polynomial 2 & Display\n");
 printf("3.Perform Polynomial Addition\n");
-printf("3.Perform Polynomial Multiplication\n");
+printf("4.Perform Polynomial Multiplication\n");
 printf("5.EXIT\n");
 printf("Enter the choice\n");
 scanf("%d",&ch);
@@ -53,7 +54,9 @@ p=head1;
 printf("Polynomial 1:\n");
 while(p!=NULL)
 {
-printf("%dx^%d\t",p->coeff,p->expo);
+printf("%dx^%d",p->coeff,p->expo);
+if(p->next!=NULL)
+printf("+");
 p=p->next;
 }
 break;
@@ -87,13 +90,16 @@ q=head2;
 printf("Polynomial 2:\n");
 while(q!=NULL)
 {
-printf("%dx^%d\t",q->coeff,q->expo);
+printf("%dx^%d",q->coeff,q->expo);
+if(q->next!=NULL)
+printf("+");
 q=q->next;
-} 
+}
 break;
 }
 case 3:
 {
+p=head1; q=head2;
 while(p!=NULL && q!=NULL)
 {
 r=(struct node*)malloc(sizeof(struct node));
@@ -129,6 +135,7 @@ r->coeff=p->coeff+q->coeff;
 p=p->next;
 q=q->next;
 }
+}
 while(p!=NULL)
 {
 r=(struct node*)malloc(sizeof(struct node));
@@ -178,8 +185,7 @@ ptr=ptr->next;
 }
 r=r->next;
 }
-r=head3;
-ptr=head3;
+r=head3; ptr=head3;
 printf("\nThe resultant polynomial:\n");
 while(r!=NULL)
 {
@@ -191,8 +197,8 @@ printf("+");
 }
 r=r->next;
 }
+head3=NULL;
 break;
-
 }
 case 4:
 {
@@ -202,50 +208,51 @@ while(p!=NULL)
 q=head2;
 while(q!=NULL)
 {
-r=(struct node*)malloc(sizeof(struct node));
-if(head3==NULL)
+m=(struct node*)malloc(sizeof(struct node));
+if(head4==NULL)
 {
-head3=r;
-tail3=r;
+head4=m;
+tail4=m;
 }
 else
 {
-tail3->next=r;
-tail3=r;
+tail4->next=m;
+tail4=m;
 }
-r->coeff=p->coeff*q->coeff;
-r->expo=p->expo+q->expo;
+m->coeff=p->coeff*q->coeff;
+m->expo=p->expo+q->expo;
 q=q->next;
 }
 p=p->next;
 }
-r=head3;
-while(r!=NULL)
+m=head4;
+while(m!=NULL)
 {
-ptr=r->next;
-while(ptr!=NULL)
-if(r->expo==ptr->expo)
+pre=m->next;
+while(pre!=NULL)
 {
-r->coeff=r->coeff+ptr->coeff;
-ptr->coeff=0;
+if(m->expo==pre->expo)
+{
+m->coeff=m->coeff+pre->coeff;
+pre->coeff=0;
 }
-ptr=ptr->next;
+pre=pre->next;
 }
-r=r->next;
+m=m->next;
 }
-r=head3;
-ptr=head3;
+m=head4; pre=head4;
 printf("\nThe resultant polynomial:\n");
-while(r!=NULL)
+while(m!=NULL)
 {
-if(r->coeff!=0)
+if(m->coeff!=0)
 {
-printf("%dx^%d",r->coeff,r->expo);
-if(r->next!=NULL)
+printf("%dx^%d",m->coeff,m->expo);
+if(m->next!=NULL)
 printf("+");
 }
-r=r->next;
+m=m->next;
 }
+head4=NULL;
 break;
 }
 case 5:
@@ -253,7 +260,16 @@ case 5:
 printf("Exiting Program\n");
 break;
 }
+default:
+{
+printf("INVALID CHOICE");
+break;
+}
 }
 }
 while(ch!=5);
 }
+
+
+
+
